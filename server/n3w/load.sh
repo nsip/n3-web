835b83e88455093ebb65bc0a484c6641a76fe661
matt-farmer <solversurfer@icloud.com>
2019-10-02 00:28:25 +1000

    added full context support and jwt access control

--- server/n3w/load.sh
+++ server/n3w/load.sh
@@ -4,37 +4,44 @@
 
 echo 'publishing school xapi data...'
 time curl -X POST -H "Content-Type: application/json"  \
--d @sample_data/xapi/xapi.json \
-http://localhost:1323/publish
+-H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJkZW1vIiwiY25hbWUiOiJteVNjaG9vbDIiLCJ1bmFtZSI6Im1hdHRmIn0.TY_C1zAFolhAhyv-uzxjiUw0eb5Wq32rqfdmxXuO7bM" \
+-d @sample_data/xapi/xapi_en.json \
+http://localhost:1323/n3/publish
 
 # echo 'publishing school naplan data...'
 # time curl -X POST -H "Content-Type: application/json"  \
+# -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJkZW1vIiwiY25hbWUiOiJteVNjaG9vbDIiLCJ1bmFtZSI6Im1hdHRmIn0.TY_C1zAFolhAhyv-uzxjiUw0eb5Wq32rqfdmxXuO7bM" \
 # -d @sample_data/naplan/sif.json \
-# http://localhost:1323/publish
+# http://localhost:1323/n3/publish
 
 echo 'publishing teaching subject data...'
 time curl -X POST -H "Content-Type: application/json"  \
+-H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJkZW1vIiwiY25hbWUiOiJteVNjaG9vbDIiLCJ1bmFtZSI6Im1hdHRmIn0.TY_C1zAFolhAhyv-uzxjiUw0eb5Wq32rqfdmxXuO7bM" \
 -d @sample_data/subjects/subjects.json \
-http://localhost:1323/publish
+http://localhost:1323/n3/publish
 
 echo 'publishing lesson data...'
 time curl -X POST -H "Content-Type: application/json"  \
+-H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJkZW1vIiwiY25hbWUiOiJteVNjaG9vbDIiLCJ1bmFtZSI6Im1hdHRmIn0.TY_C1zAFolhAhyv-uzxjiUw0eb5Wq32rqfdmxXuO7bM" \
 -d @sample_data/lessons/lessons.json \
-http://localhost:1323/publish
+http://localhost:1323/n3/publish
 
 echo 'publishing curriculum data...'
 time curl -X POST -H "Content-Type: application/json"  \
+-H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJkZW1vIiwiY25hbWUiOiJteVNjaG9vbDIiLCJ1bmFtZSI6Im1hdHRmIn0.TY_C1zAFolhAhyv-uzxjiUw0eb5Wq32rqfdmxXuO7bM" \
 -d @sample_data/curriculum/overview.json \
-http://localhost:1323/publish
+http://localhost:1323/n3/publish
 
 time curl -X POST -H "Content-Type: application/json"  \
+-H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJkZW1vIiwiY25hbWUiOiJteVNjaG9vbDIiLCJ1bmFtZSI6Im1hdHRmIn0.TY_C1zAFolhAhyv-uzxjiUw0eb5Wq32rqfdmxXuO7bM" \
 -d @sample_data/curriculum/content.json \
-http://localhost:1323/publish
+http://localhost:1323/n3/publish
 
 echo 'publishing school sif data...'
 time curl -X POST -H "Content-Type: application/json"  \
+-H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJkZW1vIiwiY25hbWUiOiJteVNjaG9vbDIiLCJ1bmFtZSI6Im1hdHRmIn0.TY_C1zAFolhAhyv-uzxjiUw0eb5Wq32rqfdmxXuO7bM" \
 -d @sample_data/sif/sif.json \
-http://localhost:1323/publish
+http://localhost:1323/n3/publish
 
 
 
