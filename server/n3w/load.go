4635b2b0f0d9d1e773c7f5ce35098bd6f14fd389
Scott Penrose <scottp@dd.com.au>
2019-10-23 15:24:45 +1100

    Reformat code

--- server/n3w/load.go
+++ server/n3w/load.go
@@ -4,19 +4,19 @@ import (
 	"encoding/csv"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
-  "io/ioutil"
-  "strings"
+	"strings"
 )
 
 func main() {
-  // Hard coded token for now to deal with special sample data
-  token := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJkZW1vIiwiY25hbWUiOiJteVNjaG9vbCIsInVuYW1lIjoibjNEZW1vIn0.VTD8C6pwbkQ32u-vvuHnxq3xijdwNTd54JAyt1iLF3I"
-  fmt.Printf("Expected token: %s\n", token)
+	// Hard coded token for now to deal with special sample data
+	token := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJkZW1vIiwiY25hbWUiOiJteVNjaG9vbCIsInVuYW1lIjoibjNEZW1vIn0.VTD8C6pwbkQ32u-vvuHnxq3xijdwNTd54JAyt1iLF3I"
+	fmt.Printf("Expected token: %s\n", token)
 
-  // Test the user exists
+	// Test the user exists
 	f, err := os.Open("sample_data/test_user.json")
 	if err != nil {
 		// handle err
@@ -27,7 +27,7 @@ func main() {
 		// handle err
 	}
 	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Authorization", "Bearer " + token)
+	req.Header.Set("Authorization", "Bearer "+token)
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
@@ -35,34 +35,34 @@ func main() {
 	}
 	defer resp.Body.Close()
 
-  b, err := ioutil.ReadAll(resp.Body)
-  if err != nil {
-      // handle err
-  }
-  // TODO - check return code = 500 or better error parsing, or json
-  if strings.Contains(string(b), "Failed to retrieve context") {
-    fmt.Printf("User does not exist, creating\n")
-    body := strings.NewReader(`userName=n3Demo&contextName=mySchool`)
-    req, err := http.NewRequest("POST", "http://localhost:1323/admin/newdemocontext", body)
-    if err != nil {
-    	// handle err
-    }
-    req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		// handle err
+	}
+	// TODO - check return code = 500 or better error parsing, or json
+	if strings.Contains(string(b), "Failed to retrieve context") {
+		fmt.Printf("User does not exist, creating\n")
+		body := strings.NewReader(`userName=n3Demo&contextName=mySchool`)
+		req, err := http.NewRequest("POST", "http://localhost:1323/admin/newdemocontext", body)
+		if err != nil {
+			// handle err
+		}
+		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 
-    resp, err := http.DefaultClient.Do(req)
-    if err != nil {
-    	// handle err
-    }
-    defer resp.Body.Close()
-    b, err := ioutil.ReadAll(resp.Body)
-    if err != nil {
-        // handle err
-    }
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			// handle err
+		}
+		defer resp.Body.Close()
+		b, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			// handle err
+		}
 
-    fmt.Printf("User Created Success: %s\n", b)
-  } else {
-    fmt.Printf("User already exists\n")
-  }
+		fmt.Printf("User Created Success: %s\n", b)
+	} else {
+		fmt.Printf("User already exists\n")
+	}
 
 	// POST to X with token
 	//  If not auth
@@ -94,35 +94,35 @@ func main() {
 			log.Fatal(err)
 		}
 
-    if (record[0] == "post_token") {
-      fmt.Printf("post_auth: %s => %s\n", record[2], record[1])
-    	f, err := os.Open(record[2])
-    	if err != nil {
-    		// handle err
-    	}
-    	defer f.Close()
-    	req, err := http.NewRequest("POST", record[1], f)
-    	if err != nil {
-    		// handle err
-    	}
-    	req.Header.Set("Content-Type", "application/json")
-    	req.Header.Set("Authorization", "Bearer " + token)
+		if record[0] == "post_token" {
+			fmt.Printf("post_auth: %s => %s\n", record[2], record[1])
+			f, err := os.Open(record[2])
+			if err != nil {
+				// handle err
+			}
+			defer f.Close()
+			req, err := http.NewRequest("POST", record[1], f)
+			if err != nil {
+				// handle err
+			}
+			req.Header.Set("Content-Type", "application/json")
+			req.Header.Set("Authorization", "Bearer "+token)
 
-    	resp, err := http.DefaultClient.Do(req)
-    	if err != nil {
-    		// handle err
-    	}
-    	defer resp.Body.Close()
+			resp, err := http.DefaultClient.Do(req)
+			if err != nil {
+				// handle err
+			}
+			defer resp.Body.Close()
 
-      b, err := ioutil.ReadAll(resp.Body)
-      if err != nil {
-          // handle err
-      }
+			b, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				// handle err
+			}
 
-      fmt.Printf("Success: %s\n", b)
-    } else {
-      fmt.Printf("Invalid method type in CSV, use post_token (val=%s)\n", record[0])
-    }
+			fmt.Printf("Success: %s\n", b)
+		} else {
+			fmt.Printf("Invalid method type in CSV, use post_token (val=%s)\n", record[0])
+		}
 	}
 
 }
